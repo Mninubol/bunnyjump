9c15eb2c1d5e3d53e8ee21c76108592b9e07acfc
Mninubol <vrvtdfg@hotmail.com>
2019-10-09 17:41:16 +0700

    BunnyJumpmid

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 var motion = Vector2(0,-1)
-export var Gravity = 10
+export var Gravity = 11
 export var SPEED = 500
 export var Jump_power = 500
 var jump_count = 0
@@ -12,8 +12,28 @@ func _physics_process(delta):
 	apply_gravity()
 	Jump()
 	walk()
+	update_animation()
 	
 	move_and_slide(motion, Vector2( 0, -1))
+	
+func update_animation():
+	if motion.y < 0:
+		$AnimatedSprite.play("jump")
+	elif motion.x < 0:
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.flip_h = true
+	elif motion.x > 0:
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.flip_h = false
+	
+	else:
+		$AnimatedSprite.play("stand")
+func is_dead():
+	
+	if position.y > 2600:
+		return true
+		
+	return false
 	
 func apply_gravity():
 	if is_on_floor():
@@ -25,8 +45,11 @@ func Jump():
 		if Input.is_action_just_pressed("jump"):
 			motion.y = -Jump_power
 			jump_count = jump_count + 1
+			$AudioStreamPlayer.play()
 	if is_on_floor():
 		jump_count = 0
+		
+
 func walk():
 	if Input.is_action_pressed("left") and not Input.is_action_just_pressed("right"):
 		motion.x = -SPEED
